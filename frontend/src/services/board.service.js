38e7474cc95c852516eaa5704aa50ad0c7bef399
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-03-21 19:57:54 +0200

    added files, merged store with group

--- frontend/src/services/board.service.js
+++ frontend/src/services/board.service.js
@@ -8,40 +8,32 @@ import { userService } from './user.service.js'
 const STORAGE_KEY = 'task'
 
 export const boardService = {
-    query,
-    getById,
-    save,
-    remove,
+    queryTask,
+    getTaskById,
+    saveTask,
+    removeTask,
     getEmptyTask,
-    addTaskMsg
+    addTaskMsg,
+    groupQuery,
+    getGroupById,
+    saveGroup,
+    removeGroup,
+    getEmptyGroup,
+    addGroupMsg,
 }
 window.cs = boardService
 
+// async function saveTask(boardId, groupId, task, activity) {
+//     const board = getById(boardId)
+//     // PUT /api/board/b123/task/t678
 
-async function query(filterBy = { txt: '', price: 0 }) {
-    return httpService.get(STORAGE_KEY, filterBy)
-
-    // var tasks = await storageService.query(STORAGE_KEY)
-    // if (filterBy.txt) {
-    //     const regex = new RegExp(filterBy.txt, 'i')
-    //     tasks = tasks.filter(task => regex.test(task.vendor) || regex.test(task.description))
-    // }
-    // if (filterBy.price) {
-    //     tasks = tasks.filter(task => task.price <= filterBy.price)
-    // }
-    // return tasks
-
-}
-function getById(taskId) {
-    // return storageService.get(STORAGE_KEY, taskId)
-    return httpService.get(`task/${taskId}`)
-}
-
-async function remove(taskId) {
-    // await storageService.remove(STORAGE_KEY, taskId)
-    return httpService.delete(`task/${taskId}`)
-}
-async function save(task) {
+//     // TODO: find the task, and update
+//     board.activities.unshift(activity)
+//     saveBoard(board)
+//     // return board
+//     // return task
+// }
+async function saveTask(task) {
     var savedTask
     if (task._id) {
         // savedTask = await storageService.put(STORAGE_KEY, task)
@@ -56,6 +48,30 @@ async function save(task) {
     return savedTask
 }
 
+async function queryTask(filterBy = { txt: '', price: 0 }) {
+    return httpService.get(STORAGE_KEY, filterBy)
+
+    // var tasks = await storageService.query(STORAGE_KEY)
+    // if (filterBy.txt) {
+    //     const regex = new RegExp(filterBy.txt, 'i')
+    //     tasks = tasks.filter(task => regex.test(task.vendor) || regex.test(task.description))
+    // }
+    // if (filterBy.price) {
+    //     tasks = tasks.filter(task => task.price <= filterBy.price)
+    // }
+    // return tasks
+
+}
+function getTaskById(taskId) {
+    // return storageService.get(STORAGE_KEY, taskId)
+    return httpService.get(`task/${taskId}`)
+}
+
+async function removeTask(taskId) {
+    // await storageService.remove(STORAGE_KEY, taskId)
+    return httpService.delete(`task/${taskId}`)
+}
+
 async function addTaskMsg(taskId, txt) {
     const savedMsg = await httpService.post(`task/${taskId}/msg`, { txt })
     return savedMsg
@@ -64,8 +80,89 @@ async function addTaskMsg(taskId, txt) {
 
 function getEmptyTask() {
     return {
-        vendor: 'Susita-' + (Date.now() % 1000),
-        price: utilService.getRandomIntInclusive(1000, 9000),
+        id: '',
+        title: '',
+        status: '',
+        priority: '',
+        text: '',
+        comments: [],
+        memberIds: [],
+        dueDate: null,
+        startDate: Date.now(),
+        byMember: {
+            _id: '',
+            username: '',
+            fullname: '',
+            imgUrl: ''
+        },
+        style: {
+            bgColor: ''
+        }
+    }
+}
+// async function saveGroup(boardId, groupId, group, activity) {
+//     const board = getById(boardId)
+//     // PUT /api/board/b123/group/t678
+
+//     // TODO: find the group, and update
+//     board.activities.unshift(activity)
+//     saveBoard(board)
+//     // return board
+//     // return group
+// }
+async function saveGroup(group) {
+    var savedGroup
+    if (group._id) {
+        // savedGroup = await storageService.put(STORAGE_KEY, group)
+        savedGroup = await httpService.put(`group/${group._id}`, group)
+
+    } else {
+        // Later, owner is set by the backend
+        group.owner = userService.getLoggedinUser()
+        // savedGroup = await storageService.post(STORAGE_KEY, group)
+        savedGroup = await httpService.post('group', group)
+    }
+    return savedGroup
+}
+
+async function queryGroup(filterBy = { txt: '', price: 0 }) {
+    return httpService.get(STORAGE_KEY, filterBy)
+
+    // var groups = await storageService.query(STORAGE_KEY)
+    // if (filterBy.txt) {
+    //     const regex = new RegExp(filterBy.txt, 'i')
+    //     groups = groups.filter(group => regex.test(group.vendor) || regex.test(group.description))
+    // }
+    // if (filterBy.price) {
+    //     groups = groups.filter(group => group.price <= filterBy.price)
+    // }
+    // return groups
+
+}
+function getGroupById(groupId) {
+    // return storageService.get(STORAGE_KEY, groupId)
+    return httpService.get(`group/${groupId}`)
+}
+
+async function removeGroup(groupId) {
+    // await storageService.remove(STORAGE_KEY, groupId)
+    return httpService.delete(`group/${groupId}`)
+}
+
+async function addGroupMsg(groupId, txt) {
+    const savedMsg = await httpService.post(`group/${groupId}/msg`, { txt })
+    return savedMsg
+}
+
+
+function getEmptyGroup() {
+    return {
+        id: 'g101',
+        'title': 'Frontend',
+        'archivedAt': null,
+        'tasks': [
+        ],
+        style: {}
     }
 }
 
--- frontend/src/store/board.store.js
+++ frontend/src/store/board.store.js
@@ -29,10 +29,12 @@ export function getActionAddTaskMsg(taskId) {
 
 export const taskStore = {
     state: {
-        tasks: []
+        tasks: [],
+        groups: [],
     },
     getters: {
         tasks({ tasks }) { return tasks },
+        groups({ groups }) { return groups },
     },
     mutations: {
         setTasks(state, { tasks }) {
@@ -53,11 +55,38 @@ export const taskStore = {
             if (!task.msgs) task.msgs = []
             task.msgs.push(msg)
         },
+        setGroups(state, { groups }) {
+            state.groups = groups
+        },
+        addGroup(state, { group }) {
+            state.groups.push(group)
+        },
+        updateGroup(state, { group }) {
+            const idx = state.groups.findIndex(c => c._id === group._id)
+            state.groups.splice(idx, 1, group)
+        },
+        removeGroup(state, { groupId }) {
+            state.groups = state.groups.filter(group => group._id !== groupId)
+        },
+        addGroupMsg(state, { groupId, msg }) {
+            const group = state.groups.find(group => group._id === groupId)
+            if (!group.msgs) group.msgs = []
+            group.msgs.push(msg)
+        },
     },
     actions: {
+        async saveTask({ commit }, { boardId, groupId, task, activity }) {
+            try {
+                board = boardService.saveTask(boardId, groupId, task, activity)
+                // commit(ACTION)
+            } catch (err) {
+                console.log('taskStore: Error in save task', err)
+                throw err
+            }
+        },
         async addTask(context, { task }) {
             try {
-                task = await boardService.save(task)
+                task = await boardService.saveTask(task)
                 context.commit(getActionAddTask(task))
                 return task
             } catch (err) {
@@ -67,7 +96,7 @@ export const taskStore = {
         },
         async updateTask(context, { task }) {
             try {
-                task = await boardService.save(task)
+                task = await boardService.saveTask(task)
                 context.commit(getActionUpdateTask(task))
                 return task
             } catch (err) {
@@ -77,7 +106,7 @@ export const taskStore = {
         },
         async loadTasks(context) {
             try {
-                const tasks = await boardService.query()
+                const tasks = await boardService.queryTask()
                 context.commit({ type: 'setTasks', tasks })
             } catch (err) {
                 console.log('taskStore: Error in loadTasks', err)
@@ -86,7 +115,7 @@ export const taskStore = {
         },
         async removeTask(context, { taskId }) {
             try {
-                await boardService.remove(taskId)
+                await boardService.removeTask(taskId)
                 context.commit(getActionRemoveTask(taskId))
             } catch (err) {
                 console.log('taskStore: Error in removeTask', err)
@@ -102,6 +131,61 @@ export const taskStore = {
                 throw err
             }
         },
-
+        async saveGroup({ commit }, { boardId, groupId, group, activity }) {
+            try {
+                board = boardService.saveGroup(boardId, groupId, group, activity)
+                // commit(ACTION)
+            } catch (err) {
+                console.log('groupStore: Error in save group', err)
+                throw err
+            }
+        },
+        async addGroup(context, { group }) {
+            try {
+                group = await boardService.saveGroup(group)
+                context.commit(getActionAddGroup(group))
+                return group
+            } catch (err) {
+                console.log('groupStore: Error in addGroup', err)
+                throw err
+            }
+        },
+        async updateGroup(context, { group }) {
+            try {
+                group = await boardService.saveGroup(group)
+                context.commit(getActionUpdateGroup(group))
+                return group
+            } catch (err) {
+                console.log('groupStore: Error in updateGroup', err)
+                throw err
+            }
+        },
+        async loadGroups(context) {
+            try {
+                const groups = await boardService.queryGroup()
+                context.commit({ type: 'setGroups', groups })
+            } catch (err) {
+                console.log('groupStore: Error in loadGroups', err)
+                throw err
+            }
+        },
+        async removeGroup(context, { groupId }) {
+            try {
+                await boardService.removeGroup(groupId)
+                context.commit(getActionRemoveGroup(groupId))
+            } catch (err) {
+                console.log('groupStore: Error in removeGroup', err)
+                throw err
+            }
+        },
+        // async addGroupMsg(context, { groupId, txt }) {
+        //     try {
+        //         const msg = await boardService.addGroupMsg(groupId, txt)
+        //         context.commit({ type: 'addGroupMsg', groupId, msg })
+        //     } catch (err) {
+        //         console.log('groupStore: Error in addGroupMsg', err)
+        //         throw err
+        //     }
+        // },
     }
 }
