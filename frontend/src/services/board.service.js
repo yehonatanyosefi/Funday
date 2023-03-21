28222e543cd3574d9fe3c6520082e4281765ab42
Dor <dortoledano210@gmail.com>
2023-03-21 22:40:41 +0200

    board service

--- frontend/src/services/board.service.js
+++ frontend/src/services/board.service.js
@@ -1,31 +1,29 @@
-
 // import { storageService } from './async-storage.service.js'
 import { httpService } from './http.service.js'
 import { utilService } from './util.service.js'
 import { userService } from './user.service.js'
 
-
 const STORAGE_KEY = 'task'
 
 export const boardService = {
-    queryTask,
-    getTaskById,
-    saveTask,
-    removeTask,
-    getEmptyTask,
-    addTaskMsg,
-    queryGroup,
-    getGroupById,
-    saveGroup,
-    removeGroup,
-    getEmptyGroup,
-    addGroupMsg,
-    queryBoard,
-    getBoardById,
-    saveBoard,
-    removeBoard,
-    getEmptyBoard,
-    addBoardMsg
+  queryTask,
+  getTaskById,
+  saveTask,
+  removeTask,
+  getEmptyTask,
+  addTaskMsg,
+  queryGroup,
+  getGroupById,
+  saveGroup,
+  removeGroup,
+  getEmptyGroup,
+  addGroupMsg,
+  queryBoard,
+  getBoardById,
+  saveBoard,
+  removeBoard,
+  getEmptyBoard,
+  addBoardMsg,
 }
 window.cs = boardService
 
@@ -40,71 +38,70 @@ window.cs = boardService
 //     // return task
 // }
 async function saveTask(task) {
-    var savedTask
-    if (task._id) {
-        savedTask = await storageService.put(STORAGE_KEY, task)
-        // savedTask = await httpService.put(`task/${task._id}`, task)
-
-    } else {
-        // Later, owner is set by the backend
-        task.owner = userService.getLoggedinUser()
-        savedTask = await storageService.post(STORAGE_KEY, task)
-        // savedTask = await httpService.post('task', task)
-    }
-    return savedTask
+  var savedTask
+  if (task._id) {
+    savedTask = await storageService.put(STORAGE_KEY, task)
+    // savedTask = await httpService.put(`task/${task._id}`, task)
+  } else {
+    // Later, owner is set by the backend
+    task.owner = userService.getLoggedinUser()
+    savedTask = await storageService.post(STORAGE_KEY, task)
+    // savedTask = await httpService.post('task', task)
+  }
+  return savedTask
 }
 
 async function queryTask(filterBy = { txt: '', price: 0 }) {
-    // return httpService.get(STORAGE_KEY, filterBy)
+  // return httpService.get(STORAGE_KEY, filterBy)
 
-    var tasks = await storageService.query(STORAGE_KEY)
-    if (filterBy.txt) {
-        const regex = new RegExp(filterBy.txt, 'i')
-        tasks = tasks.filter(task => regex.test(task.vendor) || regex.test(task.description))
-    }
-    if (filterBy.price) {
-        tasks = tasks.filter(task => task.price <= filterBy.price)
-    }
-    return tasks
-
+  var tasks = await storageService.query(STORAGE_KEY)
+  if (filterBy.txt) {
+    const regex = new RegExp(filterBy.txt, 'i')
+    tasks = tasks.filter(
+      (task) => regex.test(task.vendor) || regex.test(task.description)
+    )
+  }
+  if (filterBy.price) {
+    tasks = tasks.filter((task) => task.price <= filterBy.price)
+  }
+  return tasks
 }
 function getTaskById(taskId) {
-    return storageService.get(STORAGE_KEY, taskId)
-    // return httpService.get(`task/${taskId}`)
+  return storageService.get(STORAGE_KEY, taskId)
+  // return httpService.get(`task/${taskId}`)
 }
 
 async function removeTask(taskId) {
-    return await storageService.remove(STORAGE_KEY, taskId)
-    // return httpService.delete(`task/${taskId}`)
+  return await storageService.remove(STORAGE_KEY, taskId)
+  // return httpService.delete(`task/${taskId}`)
 }
 
 async function addTaskMsg(taskId, txt) {
-    const savedMsg = await httpService.post(`task/${taskId}/msg`, { txt })
-    return savedMsg
+  const savedMsg = await httpService.post(`task/${taskId}/msg`, { txt })
+  return savedMsg
 }
 
-
 function getEmptyTask() {
-    return {
-        id: '',
-        title: '',
-        status: '',
-        priority: '',
-        text: '',
-        comments: [],
-        memberIds: [],
-        dueDate: null,
-        startDate: Date.now(),
-        byMember: {
-            _id: '',
-            username: '',
-            fullname: '',
-            imgUrl: ''
-        },
-        style: {
-            bgColor: ''
-        }
-    }
+  return {
+    id: '',
+    title: '',
+    status: '',
+    priority: '',
+    text: '',
+    comments: [],
+    memberIds: [],
+    dueDate: null,
+    startDate: Date.now(),
+    byMember: {
+      _id: '',
+      username: '',
+      fullname: '',
+      imgUrl: '',
+    },
+    style: {
+      bgColor: '',
+    },
+  }
 }
 // async function saveGroup(boardId, groupId, group, activity) {
 //     const board = getById(boardId)
@@ -118,90 +115,112 @@ function getEmptyTask() {
 // }
 
 async function saveGroup(group) {
-    var savedGroup
-    if (group._id) {
-        savedGroup = await storageService.put(STORAGE_KEY, group)
-        // savedGroup = await httpService.put(`group/${group._id}`, group)
-
-    } else {
-        // Later, owner is set by the backend
-        group.owner = userService.getLoggedinUser()
-        savedGroup = await storageService.post(STORAGE_KEY, group)
-        // savedGroup = await httpService.post('group', group)
-    }
-    return savedGroup
+  var savedGroup
+  if (group._id) {
+    savedGroup = await storageService.put(STORAGE_KEY, group)
+    // savedGroup = await httpService.put(`group/${group._id}`, group)
+  } else {
+    // Later, owner is set by the backend
+    group.owner = userService.getLoggedinUser()
+    savedGroup = await storageService.post(STORAGE_KEY, group)
+    // savedGroup = await httpService.post('group', group)
+  }
+  return savedGroup
 }
 
 async function queryGroup(filterBy = { txt: '', price: 0 }) {
-    // return httpService.get(STORAGE_KEY, filterBy)
+  // return httpService.get(STORAGE_KEY, filterBy)
 
-    var groups = await storageService.query(STORAGE_KEY)
-    if (filterBy.txt) {
-        const regex = new RegExp(filterBy.txt, 'i')
-        groups = groups.filter(group => regex.test(group.vendor) || regex.test(group.description))
-    }
-    if (filterBy.price) {
-        groups = groups.filter(group => group.price <= filterBy.price)
-    }
-    return groups
-
+  var groups = await storageService.query(STORAGE_KEY)
+  if (filterBy.txt) {
+    const regex = new RegExp(filterBy.txt, 'i')
+    groups = groups.filter(
+      (group) => regex.test(group.vendor) || regex.test(group.description)
+    )
+  }
+  if (filterBy.price) {
+    groups = groups.filter((group) => group.price <= filterBy.price)
+  }
+  return groups
 }
 function getGroupById(groupId) {
-    return storageService.get(STORAGE_KEY, groupId)
-    // return httpService.get(`group/${groupId}`)
+  return storageService.get(STORAGE_KEY, groupId)
+  // return httpService.get(`group/${groupId}`)
 }
 
 async function removeGroup(groupId) {
-    await storageService.remove(STORAGE_KEY, groupId)
-    // return httpService.delete(`group/${groupId}`)
+  await storageService.remove(STORAGE_KEY, groupId)
+  // return httpService.delete(`group/${groupId}`)
 }
 
 async function addGroupMsg(groupId, txt) {
-    const savedMsg = await httpService.post(`group/${groupId}/msg`, { txt })
-    return savedMsg
+  const savedMsg = await httpService.post(`group/${groupId}/msg`, { txt })
+  return savedMsg
 }
-
 
 function getEmptyGroup() {
-    return {
-        id: 'g101',
-        'title': 'Frontend',
-        'archivedAt': null,
-        'tasks': [
-        ],
-        style: {}
-    }
+  return {
+    id: 'g101',
+    title: 'Frontend',
+    archivedAt: null,
+    tasks: [],
+    style: {},
+  }
 }
 
-async function queryBoard(filterBy={title:''}){
-    // return httpService.get(STORAGE_KEY, filterBy)
+async function queryBoard(filterBy = { title: '' }) {
+  // return httpService.get(STORAGE_KEY, filterBy)
 
-    var boards = await storageService.query(STORAGE_KEY)
-    if (filterBy.title) {
-        const regex = new RegExp(filterBy.txt, 'i')
-        boards = boards.filter(board => regex.test(board.title) )
-    }
-    // if (filterBy.price) {
-    //     boards = boards.filter(board => board.price <= filterBy.price)
-    // }
-    return boards
+  var boards = await storageService.query(STORAGE_KEY)
+  if (filterBy.title) {
+    const regex = new RegExp(filterBy.txt, 'i')
+    boards = boards.filter((board) => regex.test(board.title))
+  }
+  // if (filterBy.price) {
+  //     boards = boards.filter(board => board.price <= filterBy.price)
+  // }
+  return boards
 }
 
-async function getBoardById(){
+async function getBoardById(boardId) {
+  return storageService.get(STORAGE_KEY, boardId)
+}
+async function saveBoard(board) {
+    var savedBoard
+  if (board._id) {
+    savedBoard = await storageService.put(STORAGE_KEY, board)
+    // savedBoard = await httpService.put(`board/${board._id}`, board)
+  } else {
+    // Later, owner is set by the backend
+    const user = userService.getLoggedinUser()
+    const {_id,fullname,imgUrl}=user
+    board.createdBy._id=_id
+    board.createdBy.fullname=fullname
+    board.createdBy.imgUrl=imgUrl
+    savedBoard = await storageService.post(STORAGE_KEY, board)
+    // savedBoard = await httpService.post('board', board)
+  }
+  return savedBoard
+}
+async function removeBoard(boardId) {
+  return await storageService.remove(STORAGE_KEY, boardId)
 
 }
-async function saveBoard(){
-
+async function getEmptyBoard() {
+  return {
+    _id: '',
+    title: '',
+    isStarred: false,
+    archivedAt: '',
+    createdBy: {
+      _id: '',
+      fullname: '',
+      imgUrl: '',
+    },
+    style: {},
+    labels: [],
+    members: [],
+    groups: [],
+  }
 }
-async function removeBoard(){
-
-}
-async function getEmptyBoard(){
-
-}
-async function addBoardMsg(){
-
-}
-
-
 
