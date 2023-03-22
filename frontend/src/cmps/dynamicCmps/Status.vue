6a4e0d382d4330193cb4031f27ce9fdae9389cce
Dor <dortoledano210@gmail.com>
2023-03-22 21:39:11 +0200

    start status modal

--- frontend/src/cmps/dynamicCmps/Status.vue
+++ frontend/src/cmps/dynamicCmps/Status.vue
@@ -1,41 +1,52 @@
 <template>
-<div class="status" :class="statusClass">
-     {{info}}
-     <!-- <div class="folding-corner"><div class="fold"></div></div> -->
-</div>
+     <div class="status" :class="statusClass">
+          {{ info }}
+          <!-- <div class="folding-corner"><div class="fold"></div></div> -->
+          <div class="modal-container">
+               <div class="modal">
+                    <div class="working"><p>Working on it</p> </div>
+                    <div class="done"><p>Done</p> </div>
+                    <div class="stuck"><p>Stuck</p> </div>
+                    <div class="empty"><p></p> </div>
+                    <div class="empty-blue"><p></p></div>
+               </div>
+               <div class="divider"></div>
+          </div>
+     </div>
+     <div class="divider"></div>
 </template>
 
 <script>
 export default {
-emits: ['updateTask'],
+     emits: ['updateTask'],
      name: 'Status',
-props: {
-     info: String,
-},
-created() {
+     props: {
+          info: String,
+     },
+     created() {
 
-},
-data() {
-return {
-
-}
-},
-methods: {
-
-},
-computed: {
-     statusClass() {
+     },
+     data() {
           return {
-               'working': this.info === 'Working on it',
-               'done': this.info === 'Done',
-               'stuck': this.info === 'Stuck',
-               'empty': this.info === '',
+
           }
-     }
+     },
+     methods: {
 
-},
-components: {
+     },
+     computed: {
+          statusClass() {
+               return {
+                    'working': this.info === 'Working on it',
+                    'done': this.info === 'Done',
+                    'stuck': this.info === 'Stuck',
+                    'empty': this.info === '',
+               }
+          }
 
-},
+     },
+     components: {
+
+     },
 }
 </script>
