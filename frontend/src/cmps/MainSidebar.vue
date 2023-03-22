dba31a994d20fe6416a12e83175b20f29d7f7940
Dor <dortoledano210@gmail.com>
2023-03-22 10:39:35 +0200

    merging

--- frontend/src/cmps/MainSidebar.vue
+++ frontend/src/cmps/MainSidebar.vue
@@ -1,10 +1,32 @@
 <template>
      <section class="sidebar">
-          Sidebar
+          <img src="../assets/funday.png" alt="">
+          <button><WorkManagement class="svg-icon"/></button> 
+          <button><Notifications class="svg-icon"/></button> 
+          <button><Inbox class="svg-icon"/></button> 
+          <button><MyWork class="svg-icon"/></button> 
+          <button><Favorite class="svg-icon"/></button> 
+          <button><Apps class="svg-icon"/></button> 
+          <button><Invite class="svg-icon"/></button> 
+          <button><Search class="svg-icon"/></button> 
+          <button><Help class="svg-icon"/></button> 
+          <button><InstalledProducts class="svg-icon InstalledProducts"/></button> 
+          <img src="https://files.monday.com/euc1/photos/41054538/thumb/41054538-user_photo_2023_03_18_19_59_31.png?1679169572" class="person-bullet-image person-bullet-component avatar-only-person-bullet-wrapper" title="Dor Toledano" alt="Dor Toledano" aria-hidden="true">
      </section>
 </template>
 
 <script>
+import WorkManagement from '../assets/svg/WorkManagement.svg'
+import Notifications from '../assets/svg/Notifications.svg'
+import Inbox from '../assets/svg/Inbox.svg'
+import MyWork from '../assets/svg/MyWork.svg'
+import Favorite from '../assets/svg/Favorite.svg'
+import Apps from '../assets/svg/Apps.svg'
+import Invite from '../assets/svg/Invite.svg'
+import Search from '../assets/svg/Search.svg'
+import Help from '../assets/svg/Help.svg'
+import InstalledProducts from '../assets/svg/InstalledProducts.svg'
+
 export default {
 props: {},
 created() {
@@ -22,7 +44,16 @@ computed: {
 
 },
 components: {
-
+     WorkManagement,
+     Notifications,
+     Inbox,
+     MyWork,
+     Favorite,
+     Apps,
+     Invite,
+     Search,
+     Help,
+     InstalledProducts
 },
 }
 </script>
