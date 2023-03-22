dc640e8b7f9934ed6dd9c40d15f82ff8c95fd1e1
Dor <dortoledano210@gmail.com>
2023-03-22 17:12:27 +0200

    small mide bar pixel perfect

--- frontend/src/cmps/FoldingSidebar.vue
+++ frontend/src/cmps/FoldingSidebar.vue
@@ -1,26 +1,42 @@
 <template>
+    <main class="folding-bar">
+        <div>
+            <span>Workspace</span>
+            <p>Main workspace</p>
+            <div class="flex column">
+                <button>Add</button>
+                <button>Filters</button>
+                <button>Search</button>
+            </div>
+        </div>
 
+        <div class="spacer"></div>
+
+        <div>Dev</div>
+        <div>Finances</div>
+        <div>Sales & Marketing</div>
+    </main>
 </template>
 
 <script>
 export default {
-props: {},
-created() {
+    props: {},
+    created() {
 
-},
-data() {
-return {
+    },
+    data() {
+        return {
 
-}
-},
-methods: {
+        }
+    },
+    methods: {
 
-},
-computed: {
+    },
+    computed: {
 
-},
-components: {
+    },
+    components: {
 
-},
+    },
 }
 </script>
--- frontend/src/cmps/MainSidebar.vue
+++ frontend/src/cmps/MainSidebar.vue
@@ -1,26 +1,54 @@
 <template>
-     <section class="sidebar">
-          <img src="../assets/funday.png" alt="">
-          <!-- <div class="monday-style-divider surface-divider bottom monday-style-divider--horizontal"></div> -->
-          <div class="divider"></div>
-          <div class="flex column">
-               <div class="flex column">
-                    <button><WorkManagement class="svg-icon"/></button> 
-                    <button><Notifications class="svg-icon"/></button> 
-                    <button><Inbox class="svg-icon"/></button> 
-                    <button><MyWork class="svg-icon"/></button> 
-                    <button><Favorite class="svg-icon"/></button> 
+     <section class="side-bars">
+          <section class="sidebar">
+               <img src="../assets/funday.png" alt="">
+               <!-- <div class="monday-style-divider surface-divider bottom monday-style-divider--horizontal"></div> -->
+               <div class="divider"></div>
+               <div class="mid-icons flex column">
+                    <div class="inside-icons flex column">
+                         <div class="svg-wrapper selected">
+                              <a role="button">
+                                   <WorkManagement class="svg-icon" />
+                              </a>
+                         </div>
+     
+                         <button disabbled>
+                              <Notifications class="svg-icon" />
+                         </button>
+                         <button disabbled>
+                              <Inbox class="svg-icon" />
+                         </button>
+                         <button disabbled>
+                              <MyWork class="svg-icon" />
+                         </button>
+                         <button disabbled>
+                              <Favorite class="svg-icon" />
+                         </button>
+                    </div>
+                    <button disabbled>
+                         <Apps class="svg-icon" />
+                    </button>
+                    <button disabbled>
+                         <Invite class="svg-icon" />
+                    </button>
+                    <button disabbled>
+                         <Search class="svg-icon" />
+                    </button>
+                    <button disabbled>
+                         <Help class="svg-icon" />
+                    </button>
+     
                </div>
-               <button><Apps class="svg-icon"/></button> 
-               <button><Invite class="svg-icon"/></button> 
-
-          </div>
-
-          <button><Search class="svg-icon"/></button> 
-          <button><Help class="svg-icon"/></button> 
-          <button><InstalledProducts class="svg-icon InstalledProducts"/></button> 
-          <img src="https://files.monday.com/euc1/photos/41054538/thumb/41054538-user_photo_2023_03_18_19_59_31.png?1679169572" class="person-bullet-image person-bullet-component avatar-only-person-bullet-wrapper" title="Dor Toledano" alt="Dor Toledano" aria-hidden="true">
-     </section>
+               <div class="divider"></div>
+     
+               <button disabbled>
+                    <InstalledProducts class="svg-icon InstalledProducts" />
+               </button>
+               <img src="https://files.monday.com/euc1/photos/41054538/thumb/41054538-user_photo_2023_03_18_19_59_31.png?1679169572"
+                    class="profile" title="Dor Toledano" alt="Dor Toledano">
+               </section>
+               <FoldingSideBar />
+          </section>
 </template>
 
 <script>
@@ -35,35 +63,38 @@ import Search from '../assets/svg/Search.svg'
 import Help from '../assets/svg/Help.svg'
 import InstalledProducts from '../assets/svg/InstalledProducts.svg'
 import Divider from '../assets/svg/Divider.svg'
+import FoldingSideBar from '../cmps/FoldingSidebar.vue'
 
 export default {
-props: {},
-created() {
+     props: {},
+     created() {
 
-},
-data() {
-return {
+     },
+     data() {
+          return {
+               isSelected:false,
 
-}
-},
-methods: {
+          }
+     },
+     methods: {
 
-},
-computed: {
+     },
+     computed: {
 
-},
-components: {
-     WorkManagement,
-     Notifications,
-     Inbox,
-     MyWork,
-     Favorite,
-     Apps,
-     Invite,
-     Search,
-     Help,
-     InstalledProducts,
-     Divider
-},
+     },
+     components: {
+          WorkManagement,
+          Notifications,
+          Inbox,
+          MyWork,
+          Favorite,
+          Apps,
+          Invite,
+          Search,
+          Help,
+          InstalledProducts,
+          Divider,
+          FoldingSideBar
+     },
 }
 </script>
