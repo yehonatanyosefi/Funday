1aac9be8c231fa3ad5370095cc77afdb1c3279c6
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-03-24 17:38:43 +0300

    added random clr

--- frontend/src/services/board.service.js
+++ frontend/src/services/board.service.js
@@ -271,8 +271,23 @@ function getEmptyGroup() {
     title: 'Title',
     archivedAt: null,
     tasks: [getEmptyTask(), getEmptyTask(), getEmptyTask()],
-    style: {},
+    style: {
+      color: getRndColor(),
+    },
   }
+}
+
+function getRndColor() {
+  const colors = [
+    '#037f4c', '#9cd326', '#cab641', '#ffcb00', '#ff642e', '#ffadad',
+    '#ff7575', '#bb3354', '#ff158a', '#faa1f1', '#a25ddc', '#784bd1',
+    '#7e3b8a', '#401694', '#5559df', '#225091', '#579bfc', '#4eccc6',
+    '#6cf', '#68a1bd', '#9aadbd', '#808080', '#333', '#7f5347', '#d974b0',
+    '#ad967a', '#a1e3f6', '#bd816e', '#2b76e5', '#175a63', '#bda8f9',
+    '#a9bee8', '#9d99b9', '#563e3e',
+  ]
+  const randomIdx = utilService.getRandomIntInclusive(0, colors.length - 1)
+  return colors[randomIdx]
 }
 
 function getEmptyBoard() {
@@ -289,7 +304,7 @@ function getEmptyBoard() {
     style: {},
     labels: [],
     members: [],
-    groups: [getEmptyGroup()],
+    groups: [getEmptyGroup(), getEmptyGroup()],
   }
 }
 
