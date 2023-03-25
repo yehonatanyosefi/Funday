73b32e7e0495125a7f7b780a9c3a59b09af8abf0
Yael B <yaeleran@gmail.com>
2023-03-26 02:04:02 +0300

    addd timeline

--- frontend/src/cmps/dynamicCmps/Timeline.vue
+++ frontend/src/cmps/dynamicCmps/Timeline.vue
@@ -1,6 +1,24 @@
 <template>
-	<div>
-		{{ info }}
+	<div class="start-end-date-container">
+		<el-date-picker
+			class="start-end-date"
+			v-model="value"
+			type="daterange"
+			range-separator="To"
+			start-placeholder="Start date"
+			end-placeholder="End date"
+			:size="size"
+			@change="change"
+			:style="dateStyles"
+		/>
+		<button
+			class="timeline-progress"
+			:class="{ 'date-not-set': !isSet }"
+			@mouseover="omouseover"
+			@mouseleave="mouseleave"
+		>
+			{{ display }}
+		</button>
 	</div>
 </template>
 
@@ -11,15 +29,69 @@ export default {
 	props: {
 		info: Object,
 	},
-	created() {},
 	data() {
-		return {}
+		return {
+			value: [],
+			size: 'small',
+			isSet: false,
+			text: '',
+		}
 	},
-	methods: {},
-	computed: {},
+	methods: {
+		change() {
+			console.log(this.value)
+		},
+		formattedDate(dateToFormat) {
+			const date = new Date(dateToFormat)
+			const day = date.getDate()
+			const monthNames = [
+				'Jan',
+				'Feb',
+				'Mar',
+				'Apr',
+				'May',
+				'Jun',
+				'Jul',
+				'Aug',
+				'Sep',
+				'Oct',
+				'Nov',
+				'Dec',
+			]
+			const month = monthNames[date.getMonth()]
+			return `${month} ${day}`
+		},
+		omouseover() {
+			console.log('omouseover')
+
+			if (!this.isSet) this.text = 'setDate'
+		},
+		mouseleave() {
+			console.log('mouseleave')
+			if (!this.isSet) this.text = '-'
+		},
+	},
+	computed: {
+		dateStyles() {
+			return { width: '130px', height: '100%' }
+		},
+		display() {
+			return this.text
+			// if (!this.isSet) return '-'
+			// const startDate = this.formattedDate(this.info.startDate)
+			// const dutDate = this.formattedDate(this.info.dutDate)
+			// return `${startDate} - ${dutDate}`
+		},
+	},
 	components: {},
 	created() {
 		console.log('info', this.info)
+		if (!this.info.startDate) this.isSet = false
+		this.value = [
+			new Date(this.info?.startDate),
+			new Date(this.info?.dueDate || this.info?.startDate || Date.now()),
+		]
+		console.log('value', this.value)
 	},
 }
 </script>
