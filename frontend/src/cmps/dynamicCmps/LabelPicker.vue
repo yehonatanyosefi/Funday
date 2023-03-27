a7d63f067b7a604e7e0fd83d533481c740a7fbe0
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-03-27 19:31:50 +0300

    added sticky

--- frontend/src/cmps/dynamicCmps/LabelPicker.vue
+++ frontend/src/cmps/dynamicCmps/LabelPicker.vue
@@ -3,7 +3,7 @@
           <div class="triangle"></div>
           <div class="modal-container">
                <div v-for="(label, idx) in labels" :key="idx" :class="Object.keys(label)[0]"
-                    @click="$emit('saveTask', Object.values(label)[0])">
+                    @click="saveTask(Object.values(label)[0])">
                     <p >
                          <p class="label-name">{{ Object.values(label)[0] }}</p> 
                          <Alert v-if="Object.values(label)[0] === 'Critical'" class="svg-icon " height="15px" width="15px" />
@@ -19,32 +19,31 @@
 </template>
 
 <script>
-import Edit from '../../assets/svg/Edit.svg'
-import Alert from '../../assets/svg/Alert.svg'
+import Edit from "../../assets/svg/Edit.svg";
+import Alert from "../../assets/svg/Alert.svg";
 
 export default {
-     emits: ['saveTask', 'closeModal'],
-     props: {
-          labels: Array,
-     },
-     created() {
-
-     },
-     data() {
-          return {
-          }
-     },
-     methods: {
-          closeModal() {
-               this.$emit('closeModal')
-          },
-     },
-     computed: {
-
-     },
-     components: {
-          Edit,
-          Alert
-     },
-}
+  emits: ["saveTask", "closeModal"],
+  props: {
+    labels: Array,
+  },
+  created() {},
+  data() {
+    return {}
+  },
+  methods: {
+    closeModal() {
+      this.$emit("closeModal")
+    },
+    saveTask(payload) {
+     this.closeModal()
+     this.$emit("saveTask", payload)
+    },
+  },
+  computed: {},
+  components: {
+    Edit,
+    Alert,
+  },
+};
 </script>
