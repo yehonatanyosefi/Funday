89e987dd9d11449b70a4b37d4c7aac83843070dd
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-04-01 13:35:42 +0300

    added cursor pointer

--- Todos.js
+++ Todos.js
@@ -1,28 +1,31 @@
 kanban - Ui + drag and drop
 infinity lines
+blank in kanban
 fix update group / task attributes to update locally first
-
 mobile drag not working
+fix guest not logging sometimes or refresh not working
+user messages scss
+
 dashboard spacing, height and row -> columns
 dashboard legend reconsider, update dashboard
-input with "New Task" auto select
-user messages ccss
-try catch
-add task input start location
-content editable
 demo data dates - still not working
 modals in kanban
+try catch with error messages
+
+title input in collapsed
+debounce savew
+content editable title to make the color picker not jump
+input with "New Task" auto select
+add task input start location
 demo data guest - include guest not on invite
 GUEST - MGFDS@#RSDD1
 focus within, when modal is open in absolute
-title input in collapsed
-debounce ?
-     empty values to "blank"
-minified users, minified boards in boardlist
+minified users
 multi select duplicate
 
 yael:
 x in timeline and date
+remove cookies on logout
 
 dor:
 fix layout to see scrollbar in mobile, and avoid the layout shift before mobile
