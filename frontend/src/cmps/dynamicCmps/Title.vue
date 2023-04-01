39f3d1d11af780e2a0e01e5c3d56511c3b4f1517
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-04-01 12:41:44 +0300

    fixed some css, added progress bar sticky, added scrollbars

--- frontend/src/cmps/dynamicCmps/Title.vue
+++ frontend/src/cmps/dynamicCmps/Title.vue
@@ -1,32 +1,38 @@
 <template>
-  <div class="task-title">
-    <input
-      type="text"
-      v-model="title"
-      name="task-title"
-      class="board-input task-title-input"
-      @input="$emit('saveTask', title)"
-    />
-  </div>
+	<div class="task-title">
+		<input
+			type="text"
+			v-model="title"
+			name="task-title"
+			class="board-input task-title-input"
+			@input="$emit('saveTask', title)"
+			ref="titleInput"
+			@keyup.enter="unfocus"
+		/>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Title',
-  props: {
-    info: String,
-  },
-  emits: ['saveTask'],
-  created() {
-    this.title = this.info
-  },
-  data() {
-    return {
-      title: null,
-    }
-  },
-  methods: {},
-  computed: {},
-  components: {},
+	name: 'Title',
+	props: {
+		info: String,
+	},
+	emits: ['saveTask'],
+	created() {
+		this.title = this.info
+	},
+	data() {
+		return {
+			title: null,
+		}
+	},
+	methods: {
+		unfocus() {
+			this.$refs.titleInput.blur()
+		},
+	},
+	computed: {},
+	components: {},
 }
 </script>
