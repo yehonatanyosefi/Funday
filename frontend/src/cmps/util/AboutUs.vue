8a4aae1d0274474c1117eea62ee467c936b5a813
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-04-02 20:32:01 +0300

    added images for our about

--- frontend/src/cmps/util/AboutUs.vue
+++ frontend/src/cmps/util/AboutUs.vue
@@ -4,52 +4,46 @@
 			<div class="container">
 				<div class="container-inner">
 					<img class="circle" src="" />
-					<img class="img img1" src="" />
+					<img class="img img1" src="../../assets/img/dor.png" />
 				</div>
 			</div>
 			<div class="divider"></div>
 			<div class="name">Dor</div>
-			<div class="title">Junior Fullstack Developer</div>
+			<div class="title">Fullstack Developer</div>
 		</div>
 		<div class="person">
 			<div class="container">
 				<div class="container-inner">
 					<img class="circle" src="" />
-					<img class="img img2" src="" />
+					<img class="img img2" src="../../assets/img/yael.png" />
 				</div>
 			</div>
 			<div class="divider"></div>
 			<div class="name">Yael</div>
-			<div class="title">Junior Fullstack Developer</div>
+			<div class="title">Fullstack Developer</div>
 		</div>
 		<div class="person">
 			<div class="container">
 				<div class="container-inner">
 					<img class="circle" src="" />
-					<img class="img img3" src="" />
+					<img class="img img3" src="../../assets/img/yehonatan.png" />
 				</div>
 			</div>
 			<div class="divider"></div>
 			<div class="name">Yehonatan</div>
-			<div class="title">Junior Fullstack Developer</div>
+			<div class="title">Fullstack Developer</div>
 		</div>
 	</div>
 </template>
 
 <script>
-
 export default {
-
-	created() { },
+	created() {},
 	data() {
 		return {}
 	},
-	methods: {
-
-	},
+	methods: {},
 	computed: {},
-	components: {
-
-	},
+	components: {},
 }
 </script>
