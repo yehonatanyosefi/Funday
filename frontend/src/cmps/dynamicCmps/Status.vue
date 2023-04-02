4ee6b9767d340f9320391186a4484e45841a0f50
Yael B <yaeleran@gmail.com>
2023-04-02 17:20:11 +0300

    add divider to status/ priority

--- frontend/src/cmps/dynamicCmps/Status.vue
+++ frontend/src/cmps/dynamicCmps/Status.vue
@@ -1,61 +1,60 @@
 <template>
-<div class="status">
-     <div class="label" :class="statusClass" @click="toggleModal">
-          {{ info }}
-          <div class="folding-corner"></div>
-     </div>
-          <LabelPicker
-               v-if="isOpen"
-               class="modal"
-               :labels="labels"
-               @saveTask="$emit('saveTask',$event)"
-               @closeModal="closeModal"
-               v-click-outside="closeModal"></LabelPicker>
-</div>
+	<div class="status" :class="statusClass">
+		<div class="label" :class="{ first: isFirst }" @click="toggleModal">
+			{{ info }}
+			<div class="folding-corner"></div>
+		</div>
+		<LabelPicker
+			v-if="isOpen"
+			class="modal"
+			:labels="labels"
+			@saveTask="$emit('saveTask', $event)"
+			@closeModal="closeModal"
+			v-click-outside="closeModal"
+		></LabelPicker>
+	</div>
 </template>
 
 <script>
 import LabelPicker from './LabelPicker.vue'
 
 export default {
-     emits: ['saveTask'],
-     name: 'Status',
-     props: {
-          info: String,
-     },
-     created() {
-
-     },
-     data() {
-          return {
-               isOpen:false,
-               labels: [{working:'Working on it'}, {done:'Done'}, {stuck:'Stuck'}, {empty:''}]
-          }
-     },
-     methods: {
-          toggleModal(){
-               this.isOpen = !this.isOpen
-          },
-          openModal() {
-               this.isOpen = true
-          },
-          closeModal() {
-               this.isOpen = false
-          },
-     },
-     computed: {
-          statusClass() {
-               return {
-                    'working': this.info === 'Working on it',
-                    'done': this.info === 'Done',
-                    'stuck': this.info === 'Stuck',
-                    'empty': this.info === '',
-               }
-          }
-
-     },
-     components: {
-          LabelPicker,
-     },
+	emits: ['saveTask'],
+	name: 'Status',
+	props: {
+		info: String,
+		isFirst: Boolean,
+	},
+	created() {},
+	data() {
+		return {
+			isOpen: false,
+			labels: [{ working: 'Working on it' }, { done: 'Done' }, { stuck: 'Stuck' }, { empty: '' }],
+		}
+	},
+	methods: {
+		toggleModal() {
+			this.isOpen = !this.isOpen
+		},
+		openModal() {
+			this.isOpen = true
+		},
+		closeModal() {
+			this.isOpen = false
+		},
+	},
+	computed: {
+		statusClass() {
+			return {
+				working: this.info === 'Working on it',
+				done: this.info === 'Done',
+				stuck: this.info === 'Stuck',
+				empty: this.info === '',
+			}
+		},
+	},
+	components: {
+		LabelPicker,
+	},
 }
 </script>
