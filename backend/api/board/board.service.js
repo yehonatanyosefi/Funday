e4c56932d91bbc3f523dc2168cd219c9bb762462
Dor <dortoledano210@gmail.com>
2023-04-03 15:15:10 +0300

    openai

--- backend/api/board/board.service.js
+++ backend/api/board/board.service.js
@@ -191,6 +191,7 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration)
+
 async function gpt(boardName='Development'){
 const completion = await openai.createChatCompletion({
   model: "gpt-3.5-turbo",
