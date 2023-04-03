c0415807af21545d16bf82f63ab7cdb8b48162f7
Yael B <yaeleran@gmail.com>
2023-04-03 11:11:45 +0300

    add quill

--- frontend/src/cmps/dynamicCmps/Date.vue
+++ frontend/src/cmps/dynamicCmps/Date.vue
@@ -1,61 +1,70 @@
 <template>
-  <div>
-    <el-date-picker
-      ref="datePicker"
-      class="dynamic-date"
-      v-model="info"
-      type="date"
-      placeholder="Pick a day"
-      :style="dateStyles"
-      format="MMM DD"
-      @change="select"
-    />
-    <!-- {{ formattedDate }} -->
-  </div>
+	<div>
+		<el-date-picker
+			ref="datePicker"
+			class="dynamic-date"
+			type="date"
+			v-model="date"
+			placeholder="Pick a day"
+			:style="dateStyles"
+			format="MMM DD"
+			@change="select"
+		/>
+		<!-- {{ formattedDate }} -->
+	</div>
 </template>
 
 <script>
 export default {
-  emits: ['saveTask'],
-  name: 'Date',
-  props: {
-    info: Number,
-  },
-  created() {},
-  data() {
-    return {}
-  },
-  methods: {
-    select() {
-      const newVal = new Date(this.info).getTime()
-      this.$emit('saveTask', newVal)
-    },
-  },
-  computed: {
-    formattedDate() {
-      const date = new Date(this.info)
-      const day = date.getDate()
-      const monthNames = [
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-        'Sep',
-        'Oct',
-        'Nov',
-        'Dec',
-      ]
-      const month = monthNames[date.getMonth()]
-      return `${month} ${day}`
-    },
-    dateStyles() {
-      return {width: '130px'}
-    }
-  },
-  components: {},
+	emits: ['saveTask'],
+	name: 'Date',
+	props: {
+		info: Number,
+	},
+	created() {
+		this.date = this.info
+	},
+	data() {
+		return {
+			date: null,
+		}
+	},
+	methods: {
+		select() {
+			const newVal = new Date(this.date).getTime()
+			this.$emit('saveTask', newVal)
+		},
+	},
+	computed: {
+		formattedDate() {
+			const date = new Date(this.info)
+			const day = date.getDate()
+			const monthNames = [
+				'Jan',
+				'Feb',
+				'Mar',
+				'Apr',
+				'May',
+				'Jun',
+				'Jul',
+				'Aug',
+				'Sep',
+				'Oct',
+				'Nov',
+				'Dec',
+			]
+			const month = monthNames[date.getMonth()]
+			return `${month} ${day}`
+		},
+		dateStyles() {
+			return { width: '130px' }
+		},
+	},
+	components: {},
+	watch: {
+		info() {
+			this.date = this.info
+		},
+	},
 }
 </script>
